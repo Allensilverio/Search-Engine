9400a737f7886122d4fd613178a35f762acfb020
Allen Silverio <allensilverioo>
2023-10-05 00:32:03 -0400

    db.js & .env

--- server/database/db.js
+++ server/database/db.js
@@ -1,10 +1,12 @@
 import mysql from 'mysql2';
+import dotenv from 'dotenv';
+dotenv.config();
 
 export const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: "",
-    database: "search_engine"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
   });
 
 connection.connect((err) => {
--- server/database/script.sql
+++ server/database/script.sql
@@ -1,3 +1,4 @@
+-- Active: 1691799065576@@127.0.0.1@3306@Buscador
 
 -- Create DB
 create database Buscador;
@@ -34,13 +35,13 @@ BEGIN
 END //
 DELIMITER ;
 
-CALL spInsertPage("https://www.test.com", "Test Title", "Test Description", "https://www.test.com/icon.ico", "Test,Video Games,Books,Music,Video", "es", 100);
-CALL spInsertPage("https://www.google.com", "Google", "El motor de búsqueda más utilizado en el mundo.", "https://www.google.com/favicon.ico", "Motor de búsqueda, Google, Búsqueda en línea, Internet, Buscar", "Multi", 90);
-CALL spInsertPage("https://www.facebook.com", "Facebook", "La red social más grande y popular.", "https://www.facebook.com/favicon.ico", "Red social, Facebook, Amigos, Comunicación, Socializar", "Multi", 90);
-CALL spInsertPage("https://www.youtube.com", "YouTube", "La plataforma de videos en línea más grande y utilizada.", "https://www.youtube.com/favicon.ico", "Videos en línea, YouTube, Entretenimiento, Subir videos", "Multi", 92);
-CALL spInsertPage("https://www.amazon.com", "Amazon", "Uno de los principales sitios de comercio electrónico del mundo.", "https://www.amazon.com/favicon.ico", "Comercio electrónico, Amazon, Compras en línea, Productos, Tienda en línea", "Multi", 88);
+CALL spInsertPage("https://www.test.com", "Test Title", "abre Test Description", "https://www.test.com/icon.ico", "Test,Video Games,Books,Music,Video", "es", 100);
+CALL spInsertPage("https://www.google.com", "Google", "abre El motor de búsqueda más utilizado en el mundo.", "https://www.google.com/favicon.ico", "Motor de búsqueda, Google, Búsqueda en línea, Internet, Buscar", "Multi", 90);
+CALL spInsertPage("https://www.facebook.com", "Facebook", "abre La red social más grande y popular.", "https://www.facebook.com/favicon.ico", "Red social, Facebook, Amigos, Comunicación, Socializar", "Multi", 90);
+CALL spInsertPage("https://www.youtube.com", "YouTube", "abre La plataforma de videos en línea más grande y utilizada.", "https://www.youtube.com/favicon.ico", "Videos en línea, YouTube, Entretenimiento, Subir videos", "Multi", 92);
+CALL spInsertPage("https://www.amazon.com", "Amazon", " abre Uno de los principales sitios de comercio electrónico del mundo.", "https://www.amazon.com/favicon.ico", "Comercio electrónico, Amazon, Compras en línea, Productos, Tienda en línea", "Multi", 88);
 CALL spInsertPage("https://www.twitter.com", "Twitter", "Una plataforma de microblogging y redes sociales.", "https://www.twitter.com/favicon.ico", "Microblogging, Twitter, Tweets, Redes sociales, Seguir", "Multi", 89);
-CALL spInsertPage("https://www.wikipedia.org", "Wikipedia", "Una enciclopedia en línea de acceso abierto.", "https://www.wikipedia.org/static/favicon/wikipedia.ico", "Enciclopedia en línea, Wikipedia, Conocimiento, Colaborativo, Información", "Multi", 94);
+CALL spInsertPage("https://www.wikipedia.org", "Wikipedia", " abre Una enciclopedia en línea de acceso abierto.", "https://www.wikipedia.org/static/favicon/wikipedia.ico", "Enciclopedia en línea, Wikipedia, Conocimiento, Colaborativo, Información", "Multi", 94);
 CALL spInsertPage("https://www.netflix.com", "Netflix", "Un popular servicio de transmisión de películas y series de televisión.", "https://www.netflix.com/favicon.ico", "Transmisión de video, Netflix, Películas, Series de televisión, Entretenimiento", "Multi", 90);
 CALL spInsertPage("https://www.instagram.com", "Instagram", "Una plataforma de redes sociales centrada en imágenes y videos.", "https://www.instagram.com/favicon.ico", "Red social, Instagram, Fotos, Videos, Compartir", "Multi", 91);
 CALL spInsertPage("https://www.linkedin.com", "LinkedIn", "Una red social orientada a negocios y profesionales.", "https://www.linkedin.com/favicon.ico", "Red social profesional, LinkedIn, Perfil profesional, Networking, Empleo", "Multi", 87);
@@ -64,7 +65,7 @@ BEGIN
 END //
 DELIMITER ;
 
-CALL spGetPagesPaged(1, 5);
+CALL spGetPagesPaged('abre', 0, 5);
 
 
 CREATE PROCEDURE spGetAllPages()
@@ -75,3 +76,43 @@ SELECT * FROM Pages where Url = p_Url;
 
 CREATE PROCEDURE spGetPageByTitle(p_Title INT)
 SELECT * FROM Pages where Title = p_Title;
+
+
+
+
+
+
+
+
+
+/* Stored procedure para buscar una pagina */
+
+DROP PROCEDURE IF EXISTS spSearchPage;
+
+CREATE PROCEDURE spSearchPage(
+    IN criterio VARCHAR(255),
+    IN startRowIndex INT,
+    IN maximumRows INT
+)
+BEGIN
+    SELECT 
+        Url, 
+        Title, 
+        Description, 
+        Keywords
+    FROM 
+        Pages 
+    WHERE 
+        CONCAT(Url, Title, Description, Keywords) LIKE CONCAT('%', criterio, '%')
+        OR SOUNDEX(Url) = SOUNDEX(criterio)
+        OR SOUNDEX(Title) = SOUNDEX(criterio)
+        OR SOUNDEX(Description) = SOUNDEX(criterio)
+        OR SOUNDEX(Keywords) = SOUNDEX(criterio)
+    ORDER BY 
+        Seo_Rating DESC
+    LIMIT 
+        startRowIndex, maximumRows;
+END;
+
+
+CALL spSearchPage('social', 0, 2)
