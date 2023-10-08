8327f5466f79369b4401cb539e7e854cb6598cd4
Allen Silverio <allensilverioo>
2023-10-08 17:08:46 -0400

    final crawler

--- server/crawler/extractPageData.js
+++ server/crawler/extractPageData.js
@@ -2,6 +2,7 @@ import { load } from 'cheerio';
 import getMetaData from 'metadata-scraper';
 import { performance } from 'perf_hooks';
 import lodash from 'lodash';
+import crawlPage from './crawler.config';
 
 function asignSeoRating(objPage, loadTime) {
     let seoRating = 100
@@ -12,7 +13,7 @@ function asignSeoRating(objPage, loadTime) {
     if (objPage.language === undefined) seoRating -= 10
     if (objPage.keywords === undefined) seoRating -= 25
     if (objPage.icon === undefined) seoRating -= 25
-    if(loadTime> 1000) seoRating -= 10
+    if(loadTime> 5000) seoRating -= 10
     //console.log(loadTime);
     return seoRating;         
 }
@@ -67,7 +68,7 @@ async function extractPageMetadata(url) {
 }
 
 (async () => {
-    const objResponse = await extractPageMetadata("https://www.booking.com/hotel/do/central-park-tower.en-gb.html?aid=2311236&label=en-do-booking-desktop-2J6x5lX_xYP650OdUDaD_QS652796016219%2525253Apl%2525253Ata%2525253Ap1%2525253Ap2%2525253Aac%2525253Aap%2525253Aneg%2525253Afi%2525253Atikwd-65526620%2525253Alp9069760%2525253Ali%2525253Adec%2525253Adm&sid=17e003e3e7988ab530c3eac4aff92597&all_sr_blocks=320836302_335416901_0_0_0;checkin=2023-10-06;checkout=2023-10-09;dest_id=13746;dest_type=region;dist=0;group_adults=2;group_children=0;hapos=1;highlighted_blocks=320836302_335416901_0_0_0;hpos=1;map=1;matching_block_id=320836302_335416901_0_0_0;no_rooms=1;req_adults=2;req_children=0;room1=A%2CA;sb_price_type=total;sr_order=popularity;sr_pri_blocks=320836302_335416901_0_0_0__26903;srepoch=1696475310;srpvid=19ab1614e85600de;type=total;ucfs=1&#map_closed");
+    const objResponse = await extractPageMetadata("https://www.booking.com");
     console.log(objResponse);
 })();
 
